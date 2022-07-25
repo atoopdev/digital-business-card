13862341650b8e83d57e62dbbd86a1d1e8bd3d4f
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-25 15:27:58 -0700

    header html and styling

---
+++ business-card/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import header_img from "../images/portfolio_header_cropped.jpg"
+
+export default function Header(){
+    return <header className="header">
+        <img src={header_img} className = "header-img" alt="picture of amanda"></img>
+        <h1 className="name">Amanda Toop</h1>
+        <h2 className="title">Frontend Developer</h2>
+        <h3 className="website"><a href="https://wwww.amandatoop.com/">wwww.amandatoop.com</a></h3>
+        <div className="button-box">
+            <button className="btn email-btn">Email</button>
+            <button className="btn linkedin-btn">LinkedIn</button>
+        </div>
+    </header>
+}
