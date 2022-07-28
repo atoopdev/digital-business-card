42b9417c016ba490ab20e59fd9a91b44dc5c0074
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-28 10:31:17 -0700

    footer styling

--- business-card/src/components/Footer.js
+++ business-card/src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react"
+import TwitterIcon from "../images/square-twitter-brands.svg"
+import GitHubIcon from "../images/square-github-brands.svg"
 
 export default function Footer(){
     return <footer className="footer">
         <ul className="footer-items">
-            <li>Twitter</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>GitHub</li>
+            <li><img src={TwitterIcon} className="footer-icons grey-filter" alt="Twitter icon"></img></li>
+            <li><img src={GitHubIcon} className="footer-icons grey-filter" alt="github icon"></img></li>
         </ul>
     </footer>
 }
